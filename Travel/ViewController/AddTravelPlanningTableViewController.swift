0379bd7adebd008d96d289b9f30faea8775d3014
Jessica Ernst <99251460+Belladonnixi@users.noreply.github.com>
2022-09-16 15:08:32 +0200

    keyboard behaviour set, enter Key hides keyboard

--- Travel/ViewController/AddTravelPlanningTableViewController.swift
+++ Travel/ViewController/AddTravelPlanningTableViewController.swift
@@ -7,12 +7,25 @@
 
 import UIKit
 
-class AddTravelPlanningTableViewController: UITableViewController {
-
+class AddTravelPlanningTableViewController: UITableViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var countryTF: UITextField!
+    @IBOutlet weak var cityTF: UITextField!
+    @IBOutlet weak var startTripTF: UITextField!
+    @IBOutlet weak var endTripTF: UITextField!
+    @IBOutlet weak var travelTypeTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         settingBackground()
+        
+        countryTF.delegate = self
+        cityTF.delegate = self
+        startTripTF.delegate = self
+        endTripTF.delegate = self
+        travelTypeTF.delegate = self
+        
     }
     
     private func settingBackground() {
@@ -27,72 +40,30 @@ class AddTravelPlanningTableViewController: UITableViewController {
         imageView.addSubview(blurView)
         self.tableView.backgroundView = imageView
     }
-
+    
+    @IBAction func savingTrip(_ sender: Any) {
+    }
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return 7
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
     }
-    */
-
+    
+    // MARK: - keyboard return hides keyboard
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.endEditing(true)
+        return false
+    }
+    
 }
