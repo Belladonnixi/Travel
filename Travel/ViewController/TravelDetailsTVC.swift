7a0b4761dbd876651e68a787340198658de70e85
Jessica Ernst <99251460+Belladonnixi@users.noreply.github.com>
2022-09-24 22:22:55 +0200

    DetailsTravelPlanning view prepared

--- Travel/ViewController/TravelDetailsTVC.swift
+++ Travel/ViewController/TravelDetailsTVC.swift
@@ -15,18 +15,27 @@ class TravelDetailsTVC: UITableViewController {
     weak var delegate: TravelPlansDelegate?
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-
+    
+    @IBOutlet weak var titleTF: UITextField!
+    @IBOutlet weak var countryTF: UITextField!
+    @IBOutlet weak var cityTF: UITextField!
+    @IBOutlet weak var startTripTF: UITextField!
+    @IBOutlet weak var endTripTF: UITextField!
+    @IBOutlet weak var travelTypeTF: UITextField!
+    @IBOutlet weak var notesTV: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        notesTV.layer.cornerRadius = 5
+        notesTV.layer.borderWidth = 1
+        notesTV.layer.borderColor = UIColor.lightGray.cgColor
+        
         settingBackground()
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+//        self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
+    
     private func settingBackground() {
         
         // Add a background view to the table view
@@ -41,70 +50,15 @@ class TravelDetailsTVC: UITableViewController {
     }
     
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 8
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
