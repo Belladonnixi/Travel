ee601a0b0d7c6b39fb9a957487733cfb0fff6609
Jessica Ernst <99251460+Belladonnixi@users.noreply.github.com>
2022-09-25 20:37:28 +0200

    refactoring

--- Travel/ViewController/TravelDetailsTVC.swift
+++ Travel/ViewController/TravelDetailsTVC.swift
@@ -104,6 +104,8 @@ class TravelDetailsTVC: UITableViewController, UITextFieldDelegate, UITextViewDe
             print("Error")
         }
     }
+    
+    // background settings
     private func settingBackground() {
         
         // Add a background view to the table view
@@ -208,36 +210,4 @@ class TravelDetailsTVC: UITableViewController, UITextFieldDelegate, UITextViewDe
         return 9
     }
     
-}
-
-// MARK: - PickerView Extensions
-extension TravelDetailsTVC: UIPickerViewDataSource, UIPickerViewDelegate{
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        
-        if pickerView == travelType_picker {
-            return travelType_array.count
-        }
-        return 0
-    }
-    
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        
-        if pickerView == travelType_picker {
-            return travelType_array[row]
-        }
-        return ""
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        
-        if pickerView == travelType_picker {
-            travelTypeTF.text = travelType_array[row]
-        }
-        print("An Error occured")
-    }
 }
